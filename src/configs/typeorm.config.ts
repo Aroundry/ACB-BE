82ab4195b09349f306239e463ac9362120ec6dee
cheewr85 <cheewr85@naver.com>
2023-03-22 15:11:45 +0900

    AWS Deploy tests

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -5,7 +5,7 @@ import { Reception } from 'src/reception/reception.entity';
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.RDS_HOSTNAME,
-  port: process.env.RDS_PORT,
+  port: process.env.RDS_PORT as any,
   username: process.env.RDS_USERNAME,
   password: process.env.RDS_PASSWORD,
   database: process.env.RDS_DB_NAME,
