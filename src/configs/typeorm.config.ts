dfdf0b04670577d96e9d571799d3793dd689d950
cheewr85 <cheewr85@naver.com>
2023-03-23 16:27:34 +0900

    AWS Deploys Tests

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -10,7 +10,7 @@ export const typeORMConfig: TypeOrmModuleOptions = {
   password: process.env.RDS_PASSWORD,
   database: process.env.RDS_DB_NAME,
   entities: [User, Reception],
-  synchronize: false,
+  synchronize: true,
   ssl: {
     rejectUnauthorized: false,
   },
